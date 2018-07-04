9ac176be9b49f8ba1e4c771885be56445ebd4e0d
ikholiavko <iaroslavk@earnix.com>
2018-07-04 12:42:16 +0300

    Fix in OpenFileName structure. Changed String to WString type which wraps by JNA. Changed version to 1.1

--- api/src/main/java/jnafilechooser/api/WindowsFileChooser.java
+++ api/src/main/java/jnafilechooser/api/WindowsFileChooser.java
@@ -202,7 +202,7 @@ public class WindowsFileChooser
 		params.nMaxFile = bufferLength;
 
 		if (currentDirectory != null) {
-			params.lpstrInitialDir = currentDirectory.getAbsolutePath();
+			params.lpstrInitialDir = new WString(currentDirectory.getAbsolutePath());
 		}
 
 		// build filter string if filters were specified
